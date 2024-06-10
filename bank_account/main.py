8a8bc786ff98499ad5338c15fdd1ac5dec501248
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2024-06-09 23:32:42 -0300

    feat(bank basic functions): Implement deposit, withdrawal, and statement functions
    - Add functionality for depositing funds into the account
    - Implement the ability to withdraw funds
    - Include a statement function to display transaction history and
      current balance

--- bank_account/main.py
+++ bank_account/main.py
@@ -1,68 +1,85 @@
 from datetime import datetime
 
 menu = """
-# 
-[d] Depósito
-[s] Saque
-[e] Extrato
-[q] Sair
-# 
+
+[d] Deposit 
+[w] Withdraw
+[s] Statement
+[q] Quit
+
 """
+
 WITHDRAW_LIMIT = 3
 WITHDRAW_AMOUNT_LIMIT = 500
-number_withdrawals = 0
 balance = 0.0
 transaction_history = ()
 
 
-# def create_transaction(transaction_type, value):
-#     history = ()
-    
-#     if transaction_type not in ["d", "w"]:
-#         raise ValueError("Transaction type must be [d]eposit or [w]ithdrawal")
-    
-#     transaction_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+def create_transaction(transaction_type, value):
+    if transaction_type not in ["d", "w"]:
+        raise ValueError("Transaction type must be [d]eposit or [w]ithdrawal")
 
-#     history += ((transaction_type, value, transaction_time),)
-#     return history
+    transaction_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    return ((transaction_type, value, transaction_time),)
 
-# def deposit(value):
-#     create_transaction("d", value)
-#     g balance + value
-
-
-# def withdraw(value):
-#     global balance
-
-#     create_transaction("w", value)
-#     balance -= value
-"""
-Depositos: 
-    - mostrar na operação extrato
-Saque:
-    - 3 saques diários, R$500 por saque. 
-    - Saldo atual ao fim do extrato: R$xxx.xx
-Extrato: 
-    - Exibir depositos
-    - Exibir saques
-    - Exibir total da conta
-"""
 
 while True:
     user_input = input(menu)
-# 
+
     match user_input:
-        case 'd': 
-            amount = float(input("Amount: "))
-            deposit(amount)
-            # 
-        case 'w':
-            amount = input("Amount: ")
-            withdrawal(amount) 
-            # 
-        case 'e':
-            print('extrato') ls
-        # 
-        case 'q':
-            print('sair') 
+        case "d":
+            try:
+                amount = float(input("Amount: "))
+
+                if amount < 0:
+                    raise ValueError("Invalid amount")
+
+                balance += amount
+                transaction_history += create_transaction("d", amount)
+                print("Transaction successful")
+
+            except ValueError as e:
+                print("Error:", e)
+
+        case "w":
+            try:
+                amount = float(input("Amount: "))
+
+                if amount <= 0:
+                    raise ValueError("Invalid amount")
+
+                if amount > 500:
+                    raise ValueError("The withdrawal limit is R$500,00")
+
+                if amount > balance:
+                    raise ValueError("Insufficient funds")
+
+                count_withdrawals = 0
+                today = datetime.now().strftime("%Y-%m-%d")
+
+                for type, _value, transaction_datetime in transaction_history:
+                    if type == "w" and transaction_datetime.startswith(today):
+                        count_withdrawals += 1
+
+                if count_withdrawals >= WITHDRAW_LIMIT:
+                    raise ValueError("Maximum daily withdrawals have been reached")
+
+                balance -= amount
+                transaction_history += create_transaction("w", amount)
+                print("Transaction successful")
+
+            except ValueError as e:
+                print("Error:", e)
+
+        case "s":
+            for type, value, transaction_datetime in transaction_history:
+                print(
+                    f"Operation: {type}\nValue: R$ {value:.2f}\nDate: {transaction_datetime}"
+                )
+                print("----------------------")
+            print(f"Total: R$ {balance:.2f}")
+
+        case "q":
+            print("Exiting program...")
+            break
